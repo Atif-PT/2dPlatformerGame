c0c0e139987df99e950275b1fe848eb4f29f5958
Atif-PT <hakim.atif@yahoo.co.uk>
2023-08-04 03:52:33 +0100

    Added player animations and movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,9 +6,12 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rb;
+    BoxCollider2D coll;
     SpriteRenderer sprite;
     Animator anim;
-
+    
+    [SerializeField] LayerMask jumpableGround;
+    
     float dirX = 0f;
     [SerializeField] float moveSpeed = 7f;
     [SerializeField] float jumpforce = 14f;
@@ -21,6 +24,7 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
+        coll = GetComponent<BoxCollider2D>();
 
     }
     // Update is called once per frame
@@ -29,7 +33,7 @@ public class PlayerMovement : MonoBehaviour
         dirX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.velocity = new Vector3(rb.velocity.x, jumpforce);
         }
@@ -68,7 +72,14 @@ public class PlayerMovement : MonoBehaviour
 
         anim.SetInteger("state", (int)state);
     }
+
+    bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
+    }
+
 }
+
 
 
 
